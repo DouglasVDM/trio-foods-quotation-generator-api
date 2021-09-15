c59f8167ea68e3efd974d98d16439272db79c2b5
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-09-15 19:21:11 +0200

    parsing incoming req

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 4000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 
 const { Pool } = require('pg');
