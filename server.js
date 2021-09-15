cec268ae1c03535d8eb88f71e7d0f32307ff84d7
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-09-15 16:10:21 +0200

    connect to database in environment variable

--- server.js
+++ server.js
@@ -3,8 +3,30 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+
 app.get('/', (req, res) => {
   res.send('trio-foods-quotation-generator-api')
+});
+
+aoo.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null };
+    res.render('pages/db', results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 });
 
 app.listen(PORT, () => {
