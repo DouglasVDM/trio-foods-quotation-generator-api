996c68e2814a4895daf91b55a2157324f7529be8
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-09-15 23:33:16 +0200

    add proxy server

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://trio-foods-api.herokuapp.com/',
+  credentials: true
+}));
+app.use((req, res, next) => {
+  res.headers(
+    'Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
