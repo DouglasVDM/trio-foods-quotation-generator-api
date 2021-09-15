b635c12685803c0f00fed74e5da49ebfc5d79f6b
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-09-15 18:58:38 +0200

    read data from table

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 app.get('/db', async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM test_table');
+    const result = await client.query('SELECT * FROM cape_cheese');
     const results = { 'results': (result) ? result.rows : null };
     res.render('pages/db', results);
     client.release();
