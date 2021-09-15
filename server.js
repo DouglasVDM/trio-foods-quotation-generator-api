9a31452b8fa4e929e55aa7e775bf63c465439915
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-09-15 13:20:44 +0200

    add gitignore

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, (req, res) => {
+app.get('/', (req, res) => {
+  res.send('trio-foods-quotation-generator-api')
+});
+
+app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`)
 });
