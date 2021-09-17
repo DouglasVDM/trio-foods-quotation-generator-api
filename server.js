24dd24cc59187b92741b3ecefc0263be40ed4050
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-09-17 13:44:42 +0200

    change to result

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/cape-cheese', async (req, res) => {
     const client = await pool.connect();
     const result = await client.query('SELECT * FROM cape_cheese');
     const results = { 'results': (result) ? result.rows : null };
-    res.send('/cape-cheese', results);
+    res.send('/cape-cheese', result);
     client.release();
   } catch (err) {
     console.error(err);
