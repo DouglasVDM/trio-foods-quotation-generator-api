4d7e78d103152fd889c67b9f3cfd31d20d58801a
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-09-17 11:06:21 +0200

    add quotes to user

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const pool = new Pool({
-  user: USER,
+  user: 'USER',
   host: HOST,
   database: DATABASE,
   password: PASSWORD,
